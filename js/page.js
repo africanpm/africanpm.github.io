e5c764e8511f43b154ae5e01570474d7e4b2d15c
African PM <tonyagbatajnr@gmail.com>
2025-07-28 09:17:22 +0200

    Add files via upload

---
+++ js/page.js
@@ -0,0 +1,347 @@
+// fetch headlings
+let blogPost = "";
+$.ajax({
+    url: "./json/headlines.json",
+    type: "GET",
+    async: false,
+    success:function(res){
+        blogPost = res;
+    },
+    error:function(err){
+        console.log(err)
+    }
+});
+
+// parameter url
+if (pages != null && desc != null && pages != "" && desc != "") {
+    // load page and descrption = detailed contacts and policy
+    getAppPage(pages, desc);
+}
+else if (nav != null && nav != "") {
+    // navigate app nav bars
+    getNavPage(nav)
+}
+else if (search != null && search != "") {
+    // search post
+    getSearchPage(search)
+}
+else{
+    // 
+   landingPage();
+}
+
+// 
+function getAppPage(folder, name) {
+    let x = `
+        <p>${folder}</p>
+        <p>${name}</p>
+    `
+    $("#app_main").html(x)
+}
+
+
+// nav page
+function getNavPage(x) {
+    // implement pagination later
+    
+    let pages = "";
+    
+    for (let i = 0; i < blogPost.length; i++) {
+
+        if (blogPost[i].category == x) {
+            pages += `
+                <li>
+                    <a href="#${blogPost[i].id}" onclick="getHashTageLink(this)">
+                        <h4>${blogPost[i].headline}</h4>
+                        <img src="./gallery/${blogPost[i].category}/${blogPost[i].coverPhoto}" alt="Headline">
+                    </a>
+                </li>
+            `;
+        }
+        
+    }
+
+    let navpage = `
+        <section class="sub_mid_pages">
+
+        <button id="go_back_page"><a href="./">&#10094; <span>Back</span></a></button>
+        
+        <h2>${x}</h2>
+
+        <ul>${pages}</ul>
+        </section>
+    `;
+     $("#app_main").html(navpage)
+}
+
+
+// search page
+function getSearchPage(x) {
+    // loop json
+
+    let search = "";
+
+    for (let i = 0; i < blogPost.length; i++) {
+        
+        if (blogPost[i].headline.toLowerCase().match(x.toLowerCase())) {
+
+            search += `
+                <li>
+                    <a href="./?#${blogPost[i].id}">
+                        <section>
+                            <h4>${blogPost[i].headline}</h4>
+                            <img src="./gallery/${blogPost[i].category}/${blogPost[i].coverPhoto}" alt="Headline">
+                        </section>
+
+                        <q>
+                            ${blogPost[i].quote}
+                        </q>
+                    </a>
+                </li>            
+            `;
+        }
+        
+    }
+
+    setTimeout(() => {
+
+        if (search == "") {
+            let textArray = x.split(" ");
+
+        for (let j = 0; j < textArray.length; j++) {
+
+            for (let k = 0; k < blogPost.length; k++) {
+
+                if (blogPost[k].headline.toLowerCase().includes(textArray[j].toLowerCase()) || blogPost[k].quote.toLowerCase().includes(textArray[j].toLowerCase()) || blogPost[k].category.toLowerCase().includes(textArray[j].toLowerCase())) {
+
+                    search += `
+                        <li>
+                            <a href="./?#${blogPost[i].id}">
+                                <section>
+                                    <h4>${blogPost[k].headline}</h4>
+                                    <img src="./gallery/${blogPost[k].category}/${blogPost[k].coverPhoto}" alt="Headline">
+                                </section>
+
+                                <q>
+                                    ${blogPost[k].quote}
+                                </q>
+                            </a>
+                        </li>            
+                    `;
+                    
+                }
+            }
+        }
+
+        }
+       
+
+        // 
+    $("#app_main").html(`
+
+        <button id="go_back_page">
+            <a href="./">&#10094; <span>Back</span></a>
+        </button>
+
+        <ol id="search_result_container">
+
+            <h2>Search Result</h2>
+
+            <div>
+                ${search}
+            </div>
+
+        </ol>
+    `)
+    }, 1000);
+
+    $("#nav_form_input").val(x.toLowerCase())
+}
+
+
+// landing page
+function landingPage() {
+
+    let appendPost = "";
+
+    for (let i = 1; i < 5; i++) {
+        appendPost += `
+            <li>
+                <a href="#${blogPost[i].id}" onclick="getHashTageLink(this)">
+                    <img src="./gallery/${blogPost[i].category}/${blogPost[i].coverPhoto}" alt="Headline">
+                    <h4>${blogPost[i].headline.slice(0, 20)} <i style="font-weight:500; font-size:smaller; color:gray">...</i></h4> 
+                </a>
+            </li>
+        `;  
+    }
+
+    // 
+    let politicalPost = "";
+    let economicsPost = "";
+
+    let politicsCounter = 0;
+    let economicsCounter = 0
+
+    const moreTage = `<i style="color:gray; font-weight:500; font-size:smaller"> ...more</i>`;
+
+    for (let j = 0; j < blogPost.length; j++) {
+
+        if (blogPost[j].category == "politics" && politicsCounter < 5) {
+            politicalPost += `
+                <li>
+                    <a href="#${blogPost[j].id}" onclick="getHashTageLink(this)">
+                        <img src="./gallery/${blogPost[j].category}/${blogPost[j].coverPhoto}" alt="Sub Headline">
+                        <h4>${blogPost[j].headline}</h4>
+                        <p>${blogPost[j].quote.slice(0, 50)} ${moreTage}</p>
+                    </a>
+                </li>
+            `;
+            politicsCounter++
+        }
+
+        if (blogPost[j].category == "economics" && economicsCounter < 5) {
+            economicsPost += `
+                <li>
+                    <a href="#${blogPost[j].id}" onclick="getHashTageLink(this)">
+                        <img src="./gallery/${blogPost[j].category}/${blogPost[j].coverPhoto}" alt="SUb Headline">
+                        <h4>${blogPost[j].headline}</h4>
+                        <p>${blogPost[j].quote.slice(0, 50)} ${moreTage}</p>
+                    </a>
+                </li>
+            `;
+            economicsCounter++
+        }
+        
+    }
+
+    let mainPage = `
+        <section class="top_landing">
+    
+            <div class="top_main_landing">
+                <h2>Top Headline</h2>
+                <a href="#${blogPost[0].id}" onclick="getHashTageLink(this)">
+                    <img src="./gallery/${blogPost[0].category}/${blogPost[0].coverPhoto}" loading="lazy" alt="Top Headline">
+                    <h4>${blogPost[0].headline}</h4>
+                    <q>${blogPost[0].quote}</q>
+                </a>
+            </div>
+
+            <ul class="top_side_landing">
+                <h2>Sub Headlines</h2>
+                <div>${appendPost}<div>
+            </ul>
+        </section>
+
+        <br>
+        <hr>
+        <br>
+
+        <section class="mid_landing">
+            <ul>
+                <h3>Politcs</h3>
+                ${politicalPost}
+                <button><a href="./?nav=politics">More >></a></button>
+            </ul>
+
+            <ul>
+                <h3>Economy</h3>
+                ${economicsPost}
+                <button><a href="./?nav=economics">More >></a></button>
+            </ul>
+        </section>
+    `;
+
+    // 
+    const hashTag = window.location.hash.slice(1);
+    if (hashTag !="") {
+        pageLoader(hashTag)
+    }
+    else{
+        $("#app_main").html(mainPage)
+    }
+}
+
+function pageLoader(x) {
+
+    for (let i = 0; i < blogPost.length; i++) {
+
+        if (blogPost[i].id == x) {
+
+            $.get(`./pages/${blogPost[i].category}/${blogPost[i].id}.html`, (res)=>{
+                if (res) {
+                    $("#app_loading").fadeIn("slow", ()=>{
+                        
+                        $("#app_main").html(`
+
+                            <button id="go_back_page">
+                                <a href="./">&#10094; <span>Back</span></a>
+                            </button>
+
+                            <section id="my_article">${res}</section>
+                        `);
+
+                        $("#app_loading").fadeOut("slow", articleImageLoader);
+                        
+                        document.getElementById("app_main").scrollIntoView();
+                    })
+                }
+            })
+
+            return
+        }
+    } 
+}
+
+// 
+function articleImageLoader() {
+    // random number for articles photo
+    let randNum = Math.floor(Math.random() * 8);
+
+    for (let i = 0; i < blogPost.length; i++) {
+
+        if (blogPost[i].headline.replace(".", "") == $("#article_header").text().replace(".", "")) {
+            // console.log(blogPost[i])
+
+            $("#article_header").prepend(`<div id="time_stamp">${blogPost[i].date}</div>`)
+
+            $("#article_cover").attr('src', `./gallery/${blogPost[i].category}/${blogPost[i].coverPhoto}`);
+
+            $("#article_subcover").attr('src', `./gallery/${blogPost[i].category}/${blogPost[i].subPhoto}`);
+
+            $("#article_quotes").append(`<q>${blogPost[i].quote}</q>`).css({
+                "background-image":`linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url(../gallery/quotes/quotes_${randNum}.png)`
+            });
+        } 
+    }
+
+
+    setTimeout(() => {
+
+        // random json lenght
+        let randJson = Math.floor(Math.random() * blogPost.length);
+
+        if (blogPost[randJson].headline != $("#article_header").text().slice(10)) {
+            suggestedPage(randJson)
+        }
+
+        
+        function suggestedPage(randNum) {
+            $("#article_suggestion").html(`
+                <hr>
+                <p>You may also like: </p>
+                <a href="./?#${blogPost[randNum].id}" target="_blank" rel="noopener noreferrer">
+                    <img src="./gallery/${blogPost[randNum].category}/${blogPost[randNum].coverPhoto}" alt="">
+                    <h4>${blogPost[randNum].headline.slice(0, 100)} ...</h4>
+                </a>    
+            `)
+        }
+
+    }, 1000);
+
+}
+
+
+// 
+function getHashTageLink(x) {
+    pageLoader(x.getAttribute("href").slice(1))
+}
